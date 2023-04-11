6015bd89fe72c37bc788a2ee815ebe605dc8e827
Shahid Khan <57123934+Shahidkhan0786@users.noreply.github.com>
2023-04-11 12:32:22 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.status.json({
+        status: "okk",
+        message:"app is running"
+    })
+})
+
+app.get('/home', (req,res)=>{
+    res.status.json({
+        status: "okk",
+        message:"homeee"
+    })
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`App is up and running on port ${PORT}`)
+})
